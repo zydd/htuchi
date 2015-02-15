28d3601454b472f95c3fa6e7275f3e33383e396c
Gabriel Machado <gabriel_machado@live.com>
2015-02-15 00:33:25 -0200

    added queue to abstract_layer

--- abstract_layer.cpp
+++ abstract_layer.cpp
@@ -1,17 +1,25 @@
 #include "abstract_layer.h"
 #include "event_loop.h"
 
+abstract_layer::abstract_layer(bool queue_above, bool queue_below)
+    : _enable_queue_above(queue_above),
+      _enable_queue_below(queue_below)
+{ }
+
+
 abstract_layer::~abstract_layer()
 { }
 
 void abstract_layer::setAbove(abstract_layer *layer)
 {
     _above = layer;
+    above_inserted();
 }
 
 void abstract_layer::setBelow(abstract_layer *layer)
 {
     _below = layer;
+    below_inserted();
 }
 
 void abstract_layer::removeAbove(abstract_layer* layer)
@@ -24,12 +32,58 @@ void abstract_layer::removeBelow(abstract_layer* layer)
     if (_below == layer) _below = nullptr;
 }
 
+void abstract_layer::enable_queue_above(bool enable)
+{
+    _enable_queue_above = enable;
+    if (!enable)
+        _queue_above.clear();
+}
+
+void abstract_layer::enable_queue_below(bool enable)
+{
+    _enable_queue_below = enable;
+    if (!enable)
+        _queue_below.clear();
+}
+
+void abstract_layer::above_inserted()
+{
+    std::lock_guard<std::mutex> lock_guard(_mutex);
+    if (_enable_queue_above) {
+        while (!_queue_above.empty()) {
+            _above->processUp(std::move(_queue_above.front()));
+            _queue_above.pop_front();
+        }
+    }
+}
+
+void abstract_layer::below_inserted()
+{
+    std::lock_guard<std::mutex> lock_guard(_mutex);
+    if (_enable_queue_below) {
+        while (!_queue_below.empty()) {
+            _below->processDown(std::move(_queue_below.front()));
+            _queue_below.pop_front();
+        }
+    }
+}
+
+
 void abstract_layer::processUp(packet &&data)
 {
     if (_above) default_event_loop.post([=]() mutable { _above->processUp(std::move(data)); });
+    else if (_enable_queue_above) {
+        std::lock_guard<std::mutex> lock_guard(_mutex);
+        _queue_above.emplace_back(std::move(data));
+    }
 }
 
 void abstract_layer::processDown(packet &&data)
 {
     if (_below) default_event_loop.post([=]() mutable { _below->processDown(std::move(data)); });
+    else if (_enable_queue_below) {
+        std::lock_guard<std::mutex> lock_guard(_mutex);
+        _queue_below.emplace_back(std::move(data));
+    }
 }
+
--- abstract_layer.h
+++ abstract_layer.h
@@ -1,12 +1,16 @@
 #ifndef ABSTRACT_LAYER_H
 #define ABSTRACT_LAYER_H
 
+#include <deque>
+#include <mutex>
+
 #include "packet.h"
 #include "event_loop.h"
 
 class abstract_layer
 {
 public:
+    abstract_layer(bool queue_above = true, bool queue_below = true);
     virtual ~abstract_layer();
     void setAbove(abstract_layer *layer);
     void setBelow(abstract_layer *layer);
@@ -14,11 +18,23 @@ public:
     void removeBelow(abstract_layer *layer);
     virtual void processUp(packet &&data);
     virtual void processDown(packet &&data);
-    inline virtual void inserted() { }
 
 protected:
     abstract_layer *_above = nullptr;
     abstract_layer *_below = nullptr;
+
+    void enable_queue_above(bool enable);
+    void enable_queue_below(bool enable);
+
+private:
+    bool _enable_queue_above;
+    bool _enable_queue_below;
+    std::deque<packet> _queue_above;
+    std::deque<packet> _queue_below;
+    std::mutex _mutex;
+
+    void above_inserted();
+    void below_inserted();
 };
 
 #endif // ABSTRACT_LAYER_H
